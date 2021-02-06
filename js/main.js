e08f74aba7ba4d1c1acff459b83d4ef06d2baaa3
yoshiki <49330024+ystechnology@users.noreply.github.com>
2021-02-07 00:44:39 +0900

    init
    therd

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const cb = function (el, isIntersecting) {
+        if(isIntersecting) {
+            el.classList.add('inview');
+        }
+    }
+
+    const so = new ScrollObserver('.cover-slide', cb);
+});
